ef5aab74a39bc7a7ec5cd4f28d60852365a0ed10
naporin0624 <naporin0624@gmail.com>
2019-08-24 12:43:00 +0900

    [clean] 不要なモジュールを削除

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { GrpcMethod } from "@nestjs/microservices";
 
 @Controller()
 export class AppController {
