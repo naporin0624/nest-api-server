95b93265a2375c9927a3b354560ee1a0b8e590dd
naporin0624 <naporin0624@gmail.com>
2019-08-26 21:32:27 +0900

    [update] https support swagger-ui

--- src/addons/swagger.options.ts
+++ src/addons/swagger.options.ts
@@ -4,4 +4,5 @@ export const swaggerOptions = new DocumentBuilder()
   .setTitle("Nest API List")
   .setDescription("API一覧を表示するやつね")
   .setVersion("1.0")
+  .setSchemes("http", "https")
   .build();
