5b64c76b4a0659155e2f3d4a957deb5625766502
naporin0624 <naporin0624@gmail.com>
2019-10-10 20:25:34 +0900

    [update] NODE_ENVがproductionになるように

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require("webpack-merge");
 const common = require("./webpack.config.common");
+const webpack = require("webpack");
 
 module.exports = merge(common, {
   // productionにするとなんかバグる
   mode: "development",
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "production",
+    }),
+  ],
 });
