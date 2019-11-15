9a8c0dca6acb7076d27a4f7e530418b5a913dc1f
naporin0624 <naporin0624@gmail.com>
2019-11-15 17:28:54 +0900

    :+1: update eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["plugin:react/recommended", "prettier/react"],
+  env: {
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "no-console": "error"
+  },
+  settings: {
+    react: {
+      version: require("../package.json").dependencies.react,
+    },
+  }
+}
