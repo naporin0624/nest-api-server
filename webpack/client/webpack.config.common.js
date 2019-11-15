d59e5bfbbdf4a01f4631f22e75ed8f314373af20
naporin0624 <naporin0624@gmail.com>
2019-11-15 21:20:27 +0900

    a

--- webpack/client/webpack.config.common.js
+++ webpack/client/webpack.config.common.js
@@ -5,6 +5,7 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: ["./client/src/App.tsx", "babel-polyfill"],
+  target: "node",
   node: {
     __dirname: false,
     __filename: false,
