cbfe9a6d0163bc56ab6c373c1592237e0caef1c1
naporin0624 <naporin0624@gmail.com>
2019-11-20 01:28:30 +0900

    :+1: mongoのクエリを叩くのからtypeormへ

--- server/rfid/rfid.service.ts
+++ server/rfid/rfid.service.ts
@@ -5,83 +5,43 @@ import { Tags } from "./interfaces/tags.interface";
 import { CreateTagsDto } from "./dto/createTags.dto";
 import { subHours, subMinutes } from "date-fns";
 import { CountTags } from "./interfaces/count.interface";
+import { TagContainer, Tag } from "@/entities";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class RfidService {
   constructor(
     @InjectModel("RfidTags") private readonly tagsModel: Model<Tags>,
+    @InjectRepository(Tag) private readonly tagRepository: Repository<Tag>,
+    @InjectRepository(TagContainer)
+    private readonly tagContainerRepository: Repository<TagContainer>,
   ) {}
 
   async findAll() {
-    return this.tagsModel
-      .find()
-      .where("createdAt")
-      .gte(subHours(new Date(), 1))
-      .lt(new Date());
+    return this.tagContainerRepository.find({ relations: ["tags"] });
   }
 
   async create(createTagsDto: CreateTagsDto) {
     const createTags = new this.tagsModel(createTagsDto);
-    return createTags.save();
+    createTags.save();
+    const tagContainer = new TagContainer();
+    const tags = await this.tagRepository.create(createTagsDto.tags);
+    tagContainer.tags = tags;
+    await this.tagRepository.save(tags);
+    return this.tagContainerRepository.save(tagContainer);
   }
 
-  async findByDelete(rfidID: string) {
-    return this.tagsModel.findByIdAndDelete(rfidID);
-  }
+  async findByDelete(rfidID: string) {}
 
   async findAtTimeRange(
     startTime = subHours(new Date(), 1),
     endTime = new Date(),
-  ) {
-    return this.tagsModel
-      .find()
-      .where("createdAt")
-      .gte(startTime)
-      .lt(endTime);
-  }
+  ) {}
 
   async countReadAntennaRangeDate(
     antennaNo: number,
     startTime = subMinutes(new Date(), 1),
     endTime = new Date(),
-  ): Promise<CountTags[]> {
-    return this.tagsModel.aggregate([
-      {
-        $unwind: "$tags",
-      },
-      {
-        $match: {
-          createdAt: {
-            $gte: startTime,
-            $lt: endTime,
-          },
-          "tags.antennaNo": antennaNo,
-        },
-      },
-      {
-        $group: {
-          _id: "$_id",
-          readTime: {
-            $first: "$readTime",
-          },
-          readCount: {
-            $sum: 1,
-          },
-          createdAt: {
-            $first: "$createdAt",
-          },
-          // tags: {
-          //   $push: {
-          //     tags: "$tags",
-          //   },
-          // },
-        },
-      },
-      {
-        $sort: {
-          createdAt: 1,
-        },
-      },
-    ]);
-  }
+  ) {}
 }
