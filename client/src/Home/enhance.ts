88f234db1de719ded71e142f28888227ef986177
naporin0624 <naporin0624@gmail.com>
2019-11-19 14:08:21 +0900

    :sparkles: add ws dev

--- client/src/Home/enhance.ts
+++ client/src/Home/enhance.ts
@@ -1,6 +1,23 @@
 import * as React from "react";
+import io from "socket.io-client"
 
 export default () => {
   const [msg, setMsg] = React.useState("Welcome");
+  React.useEffect(() => {
+    const socket = io("http://localhost:8000");
+    socket.on("connect", () => {
+      console.log("Connected")
+    });
+    socket.on("add_tags", (data: number) => {
+      console.log(data)
+    })
+    socket.on('exception', (data: any) => {
+      console.log('event', data);
+    });
+    socket.on('disconnect', () => {
+      console.log('Disconnected');
+    });
+  }, [])
+
   return { msg, setMsg };
 };
