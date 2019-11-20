f41e703452645b5f4d4dd7754f336c5012d5b67f
FfrftTK <t1610415@uec.ac.jp>
2019-11-20 17:44:25 +0900

    add_encode_orm

---
+++ server/entities/Group.entity.ts
@@ -0,0 +1,40 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from "typeorm";
+import { CompanyEncode } from "./CompanyEncode.entity";
+  
+  @Entity()
+  export class Group {
+    @PrimaryGeneratedColumn()
+    id?: number;
+  
+    @Column("varchar")
+    name: string;
+  
+    @Column("int")
+    groupId: number;
+  
+    @Column("int")
+    filterId: number;
+  
+    @Column("int")
+    existingNum: number;
+  
+    @Column("varchar")
+    description?: string;
+  
+    @ManyToOne(() => CompanyEncode, companyEncode => companyEncode.groups)
+    companyEncode: CompanyEncode;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  }
+  
