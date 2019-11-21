4d5dd4d1f8b0e853afbbddf1d350611b0a0d9a51
FfrftTK <t1610415@uec.ac.jp>
2019-11-21 22:36:30 +0900

    add_store

---
+++ client/src/store/action.types.ts
@@ -0,0 +1,3 @@
+import { State } from "./state";
+import {} from "react-redux";
+import { ThunkAction } from "redux-thunk";
---
+++ client/src/store/index.ts
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, Reducer } from "redux";
+import { State } from "./state";
+
+
+const initialState: State = {
+
+}
+
+// export const setState: any = (key, value) => {
+//   return {
+//     type: "",
+//     payload: { key, value }
+//   };
+// };
+
+
+// const reducer: Reducer<State, Action> = ()
