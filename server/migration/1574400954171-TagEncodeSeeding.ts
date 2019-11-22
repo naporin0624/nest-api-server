40379e86ce11bc367ffc81f05a7b89ad88c948e9
naporin0624 <naporin0624@gmail.com>
2019-11-22 14:36:32 +0900

    :+1: add encod seeding

---
+++ server/migration/1574400954171-TagEncodeSeeding.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class TagEncodeSeeding1574400954171 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
