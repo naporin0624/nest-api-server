0b167b2a3717c2f719fb943195ee27bb39d5eb88
naporin0624 <naporin0624@gmail.com>
2019-11-23 15:52:26 +0900

    temp

--- server/migration/1574433839593-TagInfoSeeding.ts
+++ server/migration/1574433839593-TagInfoSeeding.ts
@@ -1,14 +1,11 @@
-import { getRepository, MigrationInterface, QueryRunner} from "typeorm";
-import { TagInfo } from "../entities";
-import { TagInfoSeed } from "../seeds/tagInfo.seed"
+import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
+import { TagInfo } from "@/entities";
+import { tagInfoSeed } from "@/seeds/tagInfo.seed";
 
 export class TagInfoSeeding1574433839593 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await getRepository(TagInfo).save(tagInfoSeed);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await getRepository(TagInfo).save(TagInfoSeed);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {}
 }
