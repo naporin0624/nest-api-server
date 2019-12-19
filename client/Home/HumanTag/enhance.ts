32d0eb616caac6fe0b0f797337e884d380d16210
naporin0624 <naporin0624@gmail.com>
2019-12-20 01:07:05 +0900

    :sparkles: proximity

--- client/Home/HumanTag/enhance.ts
+++ client/Home/HumanTag/enhance.ts
@@ -1,53 +1,8 @@
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { TagContainer } from "@/server/entities";
 import { api } from "@/client/lib/axios";
-import { zip } from "@/client/utils/index";
-import { format } from "date-fns";
-
 export const useEnhance = () => {
   const [humanData, setHumanData] = useState<TagContainer[]>([]);
-
-  const proximity = useCallback(
-    (containers: TagContainer[], filter: string) => {
-      return containers.map(
-        c =>
-          c.tags
-            .filter(t => t.tagInfoForLab.name === filter)
-            .map(t => ({
-              antennaNo: t.antennaNo,
-              createdAt: c.createdAt,
-              r: t.rssi,
-            }))
-            .sort((a, b) => b.r - a.r)[0],
-      );
-    },
-    [],
-  );
-
-  const humanMovementData = useMemo(
-    () =>
-      zip(
-        ["NameTag1", "NameTag2", "NameTag3", "NameTag4"].map(n =>
-          proximity(humanData, n),
-        ),
-      ).map(a => ({
-        NameTag1: (a[0] && a[0].antennaNo) || 0,
-        NameTag2: (a[1] && a[1].antennaNo) || 0,
-        NameTag3: (a[2] && a[2].antennaNo) || 0,
-        NameTag4: (a[3] && a[3].antennaNo) || 0,
-        name: format(
-          new Date(
-            (a[0] && a[0].createdAt) ||
-              (a[1] && a[1].createdAt) ||
-              (a[2] && a[2].createdAt) ||
-              (a[3] && a[3].createdAt),
-          ),
-          "HH:mm:ss",
-        ),
-      })),
-    [humanData],
-  );
-
   useEffect(() => {
     setInterval(async () => {
       const res = await api.get<TagContainer[]>(
@@ -57,5 +12,5 @@ export const useEnhance = () => {
     }, 1000 * 30 * 1);
   }, []);
 
-  return { humanMovementData };
+  return { humanData };
 };
--- client/Home/HumanTag/index.tsx
+++ client/Home/HumanTag/index.tsx
@@ -1,33 +1,13 @@
-import * as React from "react";
-import { Container, GraphLabel } from "./styles";
+import React, { Fragment } from "react";
 import { useEnhance } from "./enhance";
-import {
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  Line,
-} from "recharts";
+import { Proximity } from "./Proximity/index";
 
 export const HumanTag: React.FC = () => {
-  const { humanMovementData } = useEnhance();
-  console.log(humanMovementData);
+  const { humanData } = useEnhance();
+
   return (
-    <Container>
-      <GraphLabel>人タグ</GraphLabel>
-      <LineChart width={500} height={300} data={humanMovementData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="NameTag1" stroke="#79b636" />
-        <Line type="monotone" dataKey="NameTag2" stroke="#0e2ecd" />
-        <Line type="monotone" dataKey="NameTag3" stroke="#b061ee" />
-        <Line type="monotone" dataKey="NameTag4" stroke="#a78ec1" />
-      </LineChart>
-    </Container>
+    <Fragment>
+      <Proximity container={humanData} />
+    </Fragment>
   );
 };
