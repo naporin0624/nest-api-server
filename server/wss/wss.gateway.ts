f093de0023039e473dd073e42ce16711ccd68076
napori0624 <naporin0624@gmail.com>
2019-12-25 15:25:34 +0900

    :shower: refactor wss

--- server/wss/wss.gateway.ts
+++ server/wss/wss.gateway.ts
@@ -1,28 +1,8 @@
-import {
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-  WsResponse,
-} from "@nestjs/websockets";
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from "socket.io";
 
 @WebSocketGateway(8000)
 export class WssGateway {
   @WebSocketServer()
   wss: Server;
-
-  @SubscribeMessage("add_tags")
-  addTags(): WsResponse<void> {
-    return;
-  }
-
-  @SubscribeMessage("object_count")
-  objectCount(): WsResponse<void> {
-    return;
-  }
-
-  @SubscribeMessage("human_read_result")
-  humanReadResult(): WsResponse<void> {
-    return;
-  }
 }
