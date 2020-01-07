3feb1fdcb7cc0d8bbf35a9734cd000f5befb47d4
napori0624 <naporin0624@gmail.com>
2020-01-07 18:20:10 +0900

    :+1:

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -3,5 +3,5 @@ import io from "socket.io-client";
 const uri: string =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.WS_HOST}:${process.env.WS_PORT}`;
+    : `https://${process.env.WS_HOST}`;
 export const socket = io(uri);
